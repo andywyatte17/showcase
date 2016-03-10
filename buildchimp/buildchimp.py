3f67f2b8392e30a1bd40a50ec1fed81cb6819947
Andy Wyatt <andywyatte17@gmail.com>
2016-03-10 16:58:04 +0000

    buildchimp.py : Added QTreeView

--- buildchimp/buildchimp.py
+++ buildchimp/buildchimp.py
@@ -21,17 +21,44 @@ from PySide.QtCore import QProcess
 class Example(QtGui.QMainWindow):
     
     def __init__(self):
-        super(Example, self).__init__()        
+        super(Example, self).__init__()
         self.tabs = None
         self.processes = []
         self.initUI()
-        
-    def initUI(self):               
-        
-        mdiArea = QtGui.QTabWidget()
-        self.setCentralWidget(mdiArea)
 
-        self.tabs = [(QtGui.QTextEdit(), "1", "", -1), (QtGui.QTextEdit(), "2", "", -1)]
+    def initUI(self):
+        widget = QtGui.QWidget()
+        hb = QtGui.QHBoxLayout()
+        tv = QtGui.QTreeView()
+        self.initTreeView(tv)
+        hb.addWidget(tv)
+        self.initUI2(hb)
+        widget.setLayout(hb)
+        self.setCentralWidget(widget)
+
+    def initTreeView(self, tv):
+        from PySide.QtGui import QStandardItem, QStandardItemModel
+        tv.setSizePolicy( QtGui.QSizePolicy( QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Expanding ) )
+        tvm = QStandardItemModel()
+        parent = QStandardItem("A")
+        parent.appendRow([QStandardItem("B1")])
+        parent.appendRow([QStandardItem("B2")])
+        tvm.appendRow(parent)
+        parent = QStandardItem("C")
+        parent.appendRow([QStandardItem("D1")])
+        tvm.appendRow(parent)
+        tv.setModel(tvm)
+    
+    def initUI2(self, hb):
+        mdiArea = QtGui.QTabWidget()
+        hb.addWidget(mdiArea);
+
+        def StyledTextEdit():
+            rv = QtGui.QTextEdit()
+            rv.setStyle('{ font: "Courier New" }')
+            return rv
+        
+        self.tabs = [(StyledTextEdit(), "1", "", -1), (StyledTextEdit(), "2", "", -1)]
         mdiArea.addTab( self.tabs[0][0], self.tabs[0][1] )
         mdiArea.addTab( self.tabs[1][0], self.tabs[1][1] )
 
@@ -64,13 +91,14 @@ class Example(QtGui.QMainWindow):
         p.readyReadStandardOutput.connect( lambda: self.write_process_output(p, False) )
         p.finished.connect( lambda x : self.write_process_output(p, True) )
         #p.start("find", ["/home/andy/dev"])
-        p.start("find", ["/home/andy"])
+        #p.start("find", ["/home/andy"])
+        p.start("msbuild", [R"""D:\Projects\Vemnet068\7d\trunk\CommonCode\BuildAll\WinProj\BuildCommonCode.sln""", "/t:Build", "/p:Configuration=Release"])
 
     def write_process_output(self, process, finish):
         t = str( process.readAllStandardOutput() )
         old = self.tabs[0]
         newText = old[2] + t
-        nLines = newText.count("\n") / 10000
+        nLines = newText.count("\n") / 100
         self.tabs[0] = (old[0], old[1], newText, nLines)
         if old[3] != nLines or finish:
             old[0].append( t )
